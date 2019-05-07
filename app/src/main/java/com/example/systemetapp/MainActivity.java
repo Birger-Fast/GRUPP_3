64c8e85538596d812d8575d8ae114a79bbbd31b8
Konczen <martinkoncz@hotmail.com>
2019-05-07 12:44:29 +0200

    test1

--- app/src/main/java/com/example/systemetapp/MainActivity.java
+++ app/src/main/java/com/example/systemetapp/MainActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public class Product {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
     }
 }
